43c575a37e65e611550e127c45dd94d1b28e2e0f
weechenghuat7-collab <weechenghuat7@gmail.com>
2026-06-12 16:59:42 +0800

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -20,3 +20,25 @@ export default defineConfig(() => {
     },
   };
 });
+
+base: '/S1Business/',
+import tailwindcss from '@tailwindcss/vite';
+import react from '@vitejs/plugin-react';
+import path from 'path';
+import { defineConfig } from 'vite';
+
+export default defineConfig(() => {
+  return {
+    base: '/S1Business/',
+    plugins: [react(), tailwindcss()],
+    resolve: {
+      alias: {
+        '@': path.resolve(__dirname, '.'),
+      },
+    },
+    server: {
+      hmr: process.env.DISABLE_HMR !== 'true',
+      watch: process.env.DISABLE_HMR === 'true' ? null : {},
+    },
+  };
+});  
